01a1896760480ac7a89ba18c499688d3cee6d609
Gerard Choinka <gerard.choinka+github@gmail.com>
2018-09-21 18:03:30 +0100

    Create hanatupletree.cpp

---
+++ sketch/hanatupletree.cpp
@@ -0,0 +1,158 @@
+#include <tuple>
+#include <boost/hana.hpp>
+#include <string_view>
+
+namespace hana = boost::hana;
+
+struct M{};
+struct L{};
+struct R{};
+struct IF{};
+struct P2{};
+
+template<typename StringSource>
+constexpr int foo(StringSource ss)
+{
+    constexpr std::string_view s = ss();
+    return s.front();
+}
+using namespace hana::literals;
+
+struct ff
+{
+    template<typename T>
+    static void f1(IF, T rest)
+    {
+        if(true)
+            f1(rest[0_c], rest[0_c][0_c]);
+        else
+            f1(rest[1_c], rest[1_c][0_c]);
+    }
+
+    template<typename T>
+    static void f1(P2, T rest)
+    {
+        f1(rest[0_c], rest[0_c][0_c]);
+        f1(rest[1_c], rest[1_c][0_c]);
+    }
+
+    template<typename T>
+    static void f1(L, T rest)
+    {
+    }
+
+    template<typename T>
+    static void f1(R, T rest)
+    {
+    }
+
+    template<typename T>
+    static void f1(M, T rest)
+    {
+    }
+};
+
+struct counter
+{
+    int countM = 2;
+    int countL = 1;
+    int countR = 0;
+    int countIF = 1;
+    int countP2 = 1;
+};
+
+
+template<typename ContextT, typename TrueTreeT, typename FalseTreeT>
+void Sink(ContextT & contex, hana::tuple<IF, TrueTreeT, FalseTreeT> tup)
+{
+    if(contex.cond())
+        Sink(contex, tup[1_c]);
+    else
+        Sink(contex, tup[2_c]);
+}
+
+template<typename ContextT, typename Child0T, typename Child1T>
+void Sink(ContextT & contex, hana::tuple<P2, Child0T, Child1T> tup)
+{
+    Sink(contex, tup[1_c]);
+    Sink(contex, tup[2_c]);
+}
+
+template<typename ContextT>
+void Sink(ContextT & contex, hana::tuple<M> tup)
+{
+    contex.move();
+}
+
+template<typename ContextT>
+void Sink(ContextT & contex, hana::tuple<L> tup)
+{
+    contex.left();
+}
+
+template<typename ContextT>
+void Sink(ContextT & contex, hana::tuple<R> tup)
+{
+    contex.right();
+}
+
+
+template<typename ContextT, typename ...T>
+void Sink(ContextT & contex, hana::tuple<T...> tup)
+{
+
+}
+
+
+template<typename T>
+counter foobar(T tup,  counter c)
+{
+    boost::hana::for_each(tup, hana::overload(
+        [&](auto x) {c = foobar(x, c);},
+        [&](P2){--(c.countP2);},
+        [&](IF){--(c.countIF);},
+        [&](M){--(c.countM);},
+        [&](L){--(c.countL);},
+        [&](R){--(c.countR);}
+    ));
+    return c;
+}
+
+struct AntSim
+{
+    mutable int condC = 0;
+    mutable int moveC = 0;
+    mutable int leftC = 0 ;
+    mutable int rightC = 0;
+    bool cond() const { condC++; return true; }
+    void move() {moveC+=10;}
+    void left() {leftC+=100;}
+    void right() {rightC+=1000;}
+    int c() { return condC + moveC + leftC + rightC;  }
+};
+
+int main()
+{
+    auto b = hana::tuple<IF, 
+        hana::tuple<P2, hana::tuple<M>, hana::tuple<P2, hana::tuple<M>, hana::tuple<R>>>, 
+        hana::tuple<L>>
+    {};
+    counter co;
+    auto b2 = hana::tuple<IF, hana::tuple<hana::tuple<P2, hana::tuple<M, M>>, hana::tuple<L>>>{};
+    
+    co = foobar(b2, co);
+    //ff::f1(b2[0_c], b2[1_c]);
+//    boost::hana::for_each(b, hana::overload(
+//        [&](auto x) {},
+//       [&](P2, auto n1, auto n2){--countIF;},
+//        [&](IF, auto trueNodes, auto falseNodes){--countIF;},
+//        [&](M){--countM;},
+//        [&](L){--countL;},
+//        [&](R){--countR;}
+//    ));
+
+    AntSim as;
+    Sink(as, b);
+    return as.c();
+    
+}
