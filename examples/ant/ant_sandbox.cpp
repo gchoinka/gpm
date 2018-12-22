2bb1a985623bdc42a2f2c01686479edcb1d5706f
Gerard Choinka <gerard.choinka@gmail.com>
2018-12-22 01:47:34 +0000

    add factory to ipm tree

--- examples/ant/ant_sandbox.cpp
+++ examples/ant/ant_sandbox.cpp
@@ -131,24 +131,169 @@ std::array<typename NodeVector<ContexType>::size_type, N> getChildrenIndex(
 }
 
 template <typename ContexType>
-auto getNodes() -> decltype(auto) {
-  using namespace std::literals;
-  using NodeDesT = NodeDescription<ContexType>;
-  return std::array{
-      NodeDesT{"m"sv,
-               [](NodeVector<ContexType> const&,
-                  typename NodeVector<ContexType>::size_type,
-                  ContexType& c) { c.move(); },
-               0},
-      NodeDesT{"p2"sv,
-               [](NodeVector<ContexType> const& n,
-                  typename NodeVector<ContexType>::size_type currentNodePos,
-                  ContexType& c) {
-                 for (auto childIdx :
-                      getChildrenIndex<ContexType, 2>(n, currentNodePos))
+struct IptAntNodesDef {
+  static auto get() -> decltype(auto) {
+    using namespace std::literals;
+    using NodeDesT = NodeDescription<ContexType>;
+    return std::array{
+        NodeDesT{"m"sv,
+                 [](NodeVector<ContexType> const&,
+                    typename NodeVector<ContexType>::size_type,
+                    ContexType& c) { c.move(); },
+                 0},
+        NodeDesT{"l"sv,
+                 [](NodeVector<ContexType> const&,
+                    typename NodeVector<ContexType>::size_type,
+                    ContexType& c) { c.left(); },
+                 0},
+
+        NodeDesT{"r"sv,
+                 [](NodeVector<ContexType> const&,
+                    typename NodeVector<ContexType>::size_type,
+                    ContexType& c) { c.right(); },
+                 0},
+
+        NodeDesT{"p2"sv,
+                 [](NodeVector<ContexType> const& n,
+                    typename NodeVector<ContexType>::size_type currentNodePos,
+                    ContexType& c) {
+                   for (auto childIdx :
+                        getChildrenIndex<ContexType, 2>(n, currentNodePos))
+                     n[childIdx].behavior(n, childIdx, c);
+                 },
+                 2},
+        NodeDesT{"p3"sv,
+                 [](NodeVector<ContexType> const& n,
+                    typename NodeVector<ContexType>::size_type currentNodePos,
+                    ContexType& c) {
+                   for (auto childIdx :
+                        getChildrenIndex<ContexType, 3>(n, currentNodePos))
+                     n[childIdx].behavior(n, childIdx, c);
+                 },
+                 3},
+        NodeDesT{"if"sv,
+                 [](NodeVector<ContexType> const& n,
+                    typename NodeVector<ContexType>::size_type currentNodePos,
+                    ContexType& c) {
+                   auto childIdxArray =
+                       getChildrenIndex<ContexType, 2>(n, currentNodePos);
+
+                   auto childIdx = childIdxArray[c.is_food_in_front() ? 0 : 1];
                    n[childIdx].behavior(n, childIdx, c);
-               },
-               2}};
+                 },
+                 2}
+
+    };
+  }
+};
+
+namespace detail {
+
+template <typename ContexType, typename GetNodesDefType, typename CursorType>
+struct FactoryMapBuilder {
+  using FactoryFunc = std::function<void(CursorType&, NodeVector<ContexType>&)>;
+  using FactoryMap = frozen::unordered_map<frozen::string, FactoryFunc, 6>;
+
+  static FactoryFunc makeFactoryFunc(NodeDescription<ContexType> templateNode) {
+    return [templateNode](CursorType& tokenCursor,
+                          NodeVector<ContexType>& nodeVector) {
+      nodeVector.emplace_back(
+          Node<ContexType>{templateNode.behavior, templateNode.childrenCount});
+      for (std::size_t i = 0; i < templateNode.childrenCount; ++i) {
+        tokenCursor.next();
+        auto token = tokenCursor.token();
+        auto key = frozen::string{token.data(), token.size()};
+        factoryMap.at(key)(tokenCursor, nodeVector);
+      }
+    };
+  }
+
+  template <typename NodesT, auto... Idx>
+  static FactoryMap makeFactoryMapImpl(NodesT nodes,
+                                       std::index_sequence<Idx...>) {
+    auto toFrozenString = [](NodeDescription<ContexType> templateNode) {
+      return frozen::string{templateNode.name.data(), templateNode.name.size()};
+    };
+    return frozen::unordered_map<frozen::string, FactoryFunc,
+                                 std::tuple_size_v<NodesT>>{
+        {toFrozenString(std::get<Idx>(nodes)),
+         makeFactoryFunc(std::get<Idx>(nodes))}...};
+  }
+
+  static inline FactoryMap factoryMap = []() {
+    auto antNodes = GetNodesDefType::get();
+    return FactoryMapBuilder<ContexType, GetNodesDefType, CursorType>::
+        makeFactoryMapImpl(
+            antNodes,
+            std::make_index_sequence<std::tuple_size_v<decltype(antNodes)>>());
+  }();
+
+  static void factory(CursorType& tokenCursor,
+                      NodeVector<ContexType>& nodeVector) {
+    auto token = tokenCursor.token();
+    auto key = frozen::string{token.data(), token.size()};
+    factoryMap.at(key)(tokenCursor, nodeVector);
+  }
+};
+
+template <typename ContexType, typename GetNodesDefType>
+struct NodeDescriptionMapBilder {
+  using NameToNodeDescriptionMapType =
+      boost::container::flat_map<std::string_view, NodeDescription<ContexType>>;
+  using BehaviorToNodeDescriptionMapType =
+      boost::container::flat_map<typename Node<ContexType>::BehaviorPtr,
+                                 NodeDescription<ContexType>>;
+
+  static inline NameToNodeDescriptionMapType nameToNodeDescriptionMap = []() {
+    NameToNodeDescriptionMapType m{};
+    auto antNodesDes = GetNodesDefType::get();
+    for (auto& nDes : antNodesDes) m[nDes.name] = nDes;
+    return m;
+  }();
+
+  static inline BehaviorToNodeDescriptionMapType
+      behaviorToNodeDescriptionMapType = []() {
+        BehaviorToNodeDescriptionMapType m{};
+        auto antNodesDes = GetNodesDefType::get();
+        for (auto& nDes : antNodesDes) m[nDes.behavior] = nDes;
+        return m;
+      }();
+
+  static NodeDescription<ContexType> const& getNodeDescription(
+      std::string_view name) {
+    return NodeDescriptionMapBilder<
+        ContexType, GetNodesDefType>::nameToNodeDescriptionMap[name];
+  }
+  static NodeDescription<ContexType> const& getNodeDescription(
+      typename Node<ContexType>::BehaviorPtr behavior) {
+    return NodeDescriptionMapBilder<ContexType, GetNodesDefType>::
+        behaviorToNodeDescriptionMapType[behavior];
+  }
+};
+
+}  // namespace detail
+
+template <typename ContexType, typename GetNodesDefType,
+          typename CursorTypeType>
+NodeVector<ContexType> factory(CursorTypeType tokenCursorType) {
+  NodeVector<ContexType> nodeVector;
+  detail::FactoryMapBuilder<ContexType, GetNodesDefType,
+                            CursorTypeType>::factory(tokenCursorType,
+                                                     nodeVector);
+  return nodeVector;
+}
+
+template <typename ContexType, typename GetNodesDefType>
+NodeDescription<ContexType> getNodeDescription(std::string_view name) {
+  return detail::NodeDescriptionMapBilder<
+      ContexType, GetNodesDefType>::getNodeDescription(name);
+}
+
+template <typename ContexType, typename GetNodesDefType>
+NodeDescription<ContexType> getNodeDescription(
+    typename Node<ContexType>::BehaviorPtr behavior) {
+  return detail::NodeDescriptionMapBilder<
+      ContexType, GetNodesDefType>::getNodeDescription(behavior);
 }
 
 }  // namespace ipt
@@ -174,5 +319,8 @@ int main(int argc, char* argv[]) {
 
   using ContexType = decltype(antBoardSim);
 
-  auto nodes = ipt::getNodes<ContexType>();
+  auto nodes = ipt::IptAntNodesDef<ContexType>::get();
+  auto b =
+      ipt::factory<ContexType, ipt::IptAntNodesDef<ContexType>>(rpnTokenCursor);
+  std::cout << b.size() << "\n";
 }
